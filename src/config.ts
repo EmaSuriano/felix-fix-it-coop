b38a144b0ec143d299837ebe27ea4d93b37e1102
Ema Suriano <emanuel.suriano@gmail.com>
2026-08-26 16:49:42 +0000

    port Felix Fix-It coop: update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
-export const APP_ID = 'kaplay-coop-starter'
-export const WIDTH = 640
-export const HEIGHT = 400
-export const GRID = 16
-export const SPEED = 140
-export const HOST_COLOR = '#6ee7a8'
-export const JOIN_COLOR = '#f2b84b'
+export const APP_ID = 'felix-fix-it-coop'
+export const WIDTH = 480
+export const HEIGHT = 640
+export const GRID_COLS = 5
+export const GRID_ROWS = 4
+export const HOST_COLOR = '#3b6cff'
+export const JOIN_COLOR = '#e07020'
